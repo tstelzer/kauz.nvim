5d549b354a28ac7b6ad48e60e06bbbbb593198c3
Timm Stelzer <timmstelzer@gmail.com>
2025-01-08 17:23:34 +0100

    feat: remove mode coloring

--- lua/lualine/themes/kauz.lua
+++ lua/lualine/themes/kauz.lua
@@ -7,27 +7,27 @@ return {
     c = {bg = colors.bg_dark, fg = colors.fg}
   },
   insert = {
-    a = {bg = colors.yellow, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.bg_light, fg = colors.fg_light, gui = 'bold'},
     b = {bg = colors.bg_dark, fg = colors.fg},
     c = {bg = colors.bg_dark, fg = colors.fg}
   },
   visual = {
-    a = {bg = colors.blue, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.bg_light, fg = colors.fg_light, gui = 'bold'},
     b = {bg = colors.bg_dark, fg = colors.fg},
     c = {bg = colors.bg_dark, fg = colors.fg}
   },
   replace = {
-    a = {bg = colors.red, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.bg_light, fg = colors.fg_light, gui = 'bold'},
     b = {bg = colors.bg_dark, fg = colors.fg},
     c = {bg = colors.bg_dark, fg = colors.fg}
   },
   command = {
-    a = {bg = colors.green, fg = colors.black, gui = 'bold'},
+    a = {bg = colors.bg_light, fg = colors.fg_light, gui = 'bold'},
     b = {bg = colors.bg_dark, fg = colors.fg},
     c = {bg = colors.bg_dark, fg = colors.fg}
   },
   inactive = {
-    a = {bg = colors.gray15, fg = colors.gray3, gui = 'bold'},
+    a = {bg = colors.bg_light, fg = colors.fg_light, gui = 'bold'},
     b = {bg = colors.bg_dark, fg = colors.fg},
     c = {bg = colors.bg_dark, fg = colors.fg}
   }
